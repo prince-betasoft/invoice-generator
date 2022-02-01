f8f801c7bfbc9719c4e45e8f039e1dae6923171a
prince-betasoft <prince.betasoft@outlook.com>
2022-02-01 19:34:25 +0530

    invoice-data-fetch-api

--- generateInvoice.js
+++ generateInvoice.js
@@ -3,13 +3,13 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto("https://app.freeinvoicebuilder.com/#build", {
-    waitUntil: "networkidle2",
-  });
-  await page.pdf({ path: "invoice.pdf", format: "a4" });
+  await page.goto(
+    "http://localhost:5001/test-mode-b30/us-central1/api/fetch-sender-data",
+    {
+      waitUntil: "networkidle2",
+    }
+  );
+  await page.pdf({ path: "invoice_build.pdf", format: "a4" });
 
   await browser.close();
 })();
-
-// Firebase refrence URl
-// https://test-mode-b30-default-rtdb.europe-west1.firebasedatabase.app/
